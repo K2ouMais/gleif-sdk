9d48507ce311f76f8fe11322fca3ec3d68ce88c3
João Alves <joao.bastos.alves@outlook.com>
2023-03-21 16:04:01 +0100

    Add Parents request class

---
+++ src/Requests/LeiRecords/Lvl2RelationshipInformation/Parents/DirectParentRelationships.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace K2ouMais\Gleif\Requests\LeiRecords\Lvl2RelationshipInformation\Parents;
+
+use Saloon\Http\Request;
+use Saloon\Enums\Method;
+
+class DirectParentRelationships extends Request
+{
+    protected Method $method = Method::GET;
+
+    private string $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return '/v1/lei-records/' . $this->id . '/direct-parent-relationship';
+    }
+}
