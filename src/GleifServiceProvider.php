178d1d7585908a612ff6755d2f81096aad49b9e0
João Alves <joao.bastos.alves@outlook.com>
2023-03-21 16:07:23 +0100

    Update GleifServiceProvider

--- src/GleifServiceProvider.php
+++ src/GleifServiceProvider.php
@@ -10,16 +10,8 @@ class GleifServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('gleif-sdk')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_gleif-sdk_table')
-            ->hasCommand(GleifCommand::class);
+            ->hasConfigFile();
     }
 }
