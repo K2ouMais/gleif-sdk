9965c5e7503caaf85ac97e22fba2530991492ad3
João Alves <joao.bastos.alves@outlook.com>
2023-03-21 16:03:41 +0100

    Add Children request classes

---
+++ src/Requests/LeiRecords/Lvl2RelationshipInformation/Children/UltimateChildren.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace K2ouMais\Gleif\Requests\LeiRecords\Lvl2RelationshipInformation\Children;
+
+use Saloon\Http\Request;
+use Saloon\Enums\Method;
+
+class UltimateChildren extends Request
+{
+    protected Method $method = Method::GET;
+
+    private string $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return '/v1/lei-records/' . $this->id . '/ultimate-children';
+    }
+}
