aa8774cf21501817cc3a696dded6be37fb76a669
João Alves <joao.bastos.alves@outlook.com>
2023-03-21 16:03:55 +0100

    Add LeiIssuer request class

---
+++ src/Requests/LeiRecords/LeiIssuer/LeiIssuer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace K2ouMais\Gleif\Requests\LeiRecords\LeiIssuer;
+
+use Saloon\Http\Request;
+use Saloon\Enums\Method;
+
+class LeiIssuer extends Request
+{
+    protected Method $method = Method::GET;
+
+    private string $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return '/v1/lei-records/' . $this->id . '/lei-issuer';
+    }
+}
