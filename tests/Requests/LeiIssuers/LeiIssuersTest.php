38d22e0da149cb72e211c4551ac723ecac43bccb
João Alves <joao.bastos.alves@outlook.com>
2023-03-25 12:50:26 +0100

    Add LeiIssuers tests

---
+++ tests/Requests/LeiIssuers/LeiIssuersTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use K2ouMais\Gleif\GleifApi;
+use Symfony\Component\HttpFoundation\Response;
+use K2ouMais\Gleif\Requests\LeiIssuers\AllLeiIssuers;
+use K2ouMais\Gleif\Requests\LeiIssuers\LeiIssuerById;
+use K2ouMais\Gleif\Requests\LeiIssuers\LeiIssuerJurisdictionById;
+
+beforeEach(function () {
+    $this->connector = new GleifApi();
+});
+
+it('can request all lei issuers', function () {
+    $request = $this->connector->send(new AllLeiIssuers(5));
+
+    expect($request->status())
+        ->toBe(Response::HTTP_OK)
+        ->and($request->body())
+        ->toBeJson('It is not a Json body');
+});
+
+it('can request lei issuer by id', function () {
+    $request = $this->connector->send(new LeiIssuerById('259400L3KBYEVNHEJF55'));
+
+    expect($request->status())
+        ->toBe(Response::HTTP_OK)
+        ->and($request->body())
+        ->toBeJson('It is not a Json body');
+});
+
+it('can request lei issuer jurisdiction by id', function () {
+    $request = $this->connector->send(new LeiIssuerJurisdictionById('259400L3KBYEVNHEJF55'));
+
+    expect($request->status())
+        ->toBe(Response::HTTP_OK)
+        ->and($request->body())
+        ->toBeJson('It is not a Json body');
+});
