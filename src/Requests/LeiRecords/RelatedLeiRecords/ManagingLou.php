d92af8463aca8455b59db398e3f7d18dd27d0395
João Alves <joao.bastos.alves@outlook.com>
2023-03-25 12:50:32 +0100

    Add RelatedLeiRecords requests

---
+++ src/Requests/LeiRecords/RelatedLeiRecords/ManagingLou.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace K2ouMais\Gleif\Requests\LeiRecords\RelatedLeiRecords;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class ManagingLou extends Request
+{
+    protected Method $method = Method::GET;
+
+    private string $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return '/v1/lei-records/' . $this->id . '/managing-lou';
+    }
+}
